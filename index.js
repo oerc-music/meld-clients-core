53f21de232df239b874ab905c01e023a87372039
David M. Weigl <david.weigl@oerc.ox.ac.uk>
2017-02-10 23:43:28 +0000

    REST AJAX experiments

--- index.js
+++ index.js
@@ -6,16 +6,13 @@ window.$ = $;
 
 import Score from './components/score';
 
-const RDF_URI = "http://meld.linkedmusic.org/collection/DavidLewis-F1.ttl"
-
 class App extends Component { 
 
 	constructor(props) {
 		super(props);
 		
 		this.state = {
-			annotationGraph: [],
-			annotationGraphId: ""
+			annotationGraph: []
 		}
 /*
 		updateAnnotationGraph({rdfuri: RDF_URI}, (graph) => { 
@@ -28,12 +25,38 @@ class App extends Component {
 	}
 	
 	componentDidMount() { 
-		$.getJSON(RDF_URI).done(res => { 
-				this.setState({
-					annotationGraph: res,
-					annotationGraphId: res["@graph"][0]["@id"]
+		$.post(
+			"http://meld.linkedmusic.org/collection",
+			$.param({ "topLevelTargets": "http://meld.linkedmusic.org/mei/DavidLewis-F1.mei" })
+		).done((data, textStatus, xhr) => { 
+			let createAnnoStateUri = $.parseHTML(data)[0].getAttribute("href");
+			$.post(
+				createAnnoStateUri
+			).done((data, textStatus, xhr) => { 
+				console.log("Getting location: ", xhr.getResponseHeader("Location"));
+				$.get(
+					xhr.getResponseHeader("Location")
+
+				).done((data, textStatus, xhr) => { 
+					this.setState({
+						annotationGraph: JSON.parse(data)
+					});
 				});
-			);
+			});
+		});
+/*		$.post(CREATE_ANNOSTATE_URI).done(
+			(data, textStatus, xhr) => { 
+				console.log("Got it! ", data);
+				$.getJSON(RDF_URI).done(res => { 
+					this.setState({
+						annotationGraph: res,
+						annotationGraphId: res["@graph"][0]["@id"]
+					});
+					console.log(res);
+				})
+			}
+		);
+*/
 	}
 	
 /*
