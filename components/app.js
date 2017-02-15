e55a819b10554e5f15279ba9400a5cf77c0825e8
David M. Weigl <david.weigl@oerc.ox.ac.uk>
2017-02-15 11:37:31 +0000

    refactor slightly to tidy up top-level index

--- components/app.js
+++ components/app.js
@@ -1,9 +1,54 @@
 import React, { Component } from 'react';
+import IIIFImage from 'react-iiif-image';
+import MediaPlayer from "../components/mediaPlayer";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>MELD Companion</div>
-    );
-  }
-}
+export default class App extends Component { 
+
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			annotationGraph: {} 
+		}
+	}
+	
+		/*
+	componentDidMount() { 
+		$.post(
+			"http://meld.linkedmusic.org/collection",
+			$.param({ "topLevelTargets": "http://meld.linkedmusic.org/mei/DavidLewis-F1.mei" })
+		).done((data, textStatus, xhr) => { 
+			let createAnnoStateUri = $.parseHTML(data)[0].getAttribute("href");
+			$.post(
+				createAnnoStateUri
+			).done((data, textStatus, xhr) => { 
+				console.log("Getting location: ", xhr.getResponseHeader("Location"));
+				$.get(
+					xhr.getResponseHeader("Location")
+
+				).done((data, textStatus, xhr) => { 
+					this.setState({
+						annotationGraph: JSON.parse(data)
+					});
+				});
+			});
+		});
+	}
+		*/
+
+	render() { 
+		/*
+		if(!$.isEmptyObject(this.state.annotationGraph)) {
+			return <Score oaTarget={this.state.annotationGraph["@graph"][0]["oa:hasTarget"][0]["@id"]} />;
+		}
+		return <div>Loading...</div>;
+		*/
+		return (
+			<div>
+				<MediaPlayer />
+				<IIIFImage server="http://sanddragon.bl.uk" id="SanddragonImageService/RoyalMS" region="pct:33,20,40,70" size="200,"/>
+			</div>
+		)
+		
+	}
+};
--- index.js
+++ index.js
@@ -1,62 +1,9 @@
 import React, { Component }  from 'react';
 import ReactDOM from 'react-dom';
-import IIIFImage from 'react-iiif-image';
 import $ from 'jquery';
 
 window.$ = $;
 
-import Score from './components/score';
-import MediaPlayer from "./components/mediaPlayer";
-
-class App extends Component { 
-
-	constructor(props) {
-		super(props);
-		
-		this.state = {
-			annotationGraph: {} 
-		}
-	}
-	
-		/*
-	componentDidMount() { 
-		$.post(
-			"http://meld.linkedmusic.org/collection",
-			$.param({ "topLevelTargets": "http://meld.linkedmusic.org/mei/DavidLewis-F1.mei" })
-		).done((data, textStatus, xhr) => { 
-			let createAnnoStateUri = $.parseHTML(data)[0].getAttribute("href");
-			$.post(
-				createAnnoStateUri
-			).done((data, textStatus, xhr) => { 
-				console.log("Getting location: ", xhr.getResponseHeader("Location"));
-				$.get(
-					xhr.getResponseHeader("Location")
-
-				).done((data, textStatus, xhr) => { 
-					this.setState({
-						annotationGraph: JSON.parse(data)
-					});
-				});
-			});
-		});
-	}
-		*/
-
-	render() { 
-		/*
-		if(!$.isEmptyObject(this.state.annotationGraph)) {
-			return <Score oaTarget={this.state.annotationGraph["@graph"][0]["oa:hasTarget"][0]["@id"]} />;
-		}
-		return <div>Loading...</div>;
-		*/
-		return (
-			<div>
-				<MediaPlayer />
-				<IIIFImage server="http://sanddragon.bl.uk" id="SanddragonImageService/RoyalMS" region="pct:33,20,40,70" size="200,"/>
-			</div>
-		)
-		
-	}
-};
+import App from './components/app';
 
 ReactDOM.render(<App />, document.querySelector('.container'));
