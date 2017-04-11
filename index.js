a766e20521e2da7bfb30f3628546c8b01cf56991
David M. Weigl <david.weigl@oerc.ox.ac.uk>
2017-04-11 17:18:35 +0100

    react-router boilerplate!

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import ReduxPromise from 'redux-promise';
+import { Router, Route, browserHistory } from 'react-router'
 
 import App from './containers/app';
+import fooApp from './containers/fooapp';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(thunk, ReduxPromise)(createStore);
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
-		<App />
+		<Router history={browserHistory}> 
+			<Route path="/" component={App}/>
+		</Router>
 	</Provider>
 	, document.querySelector('.container'));
