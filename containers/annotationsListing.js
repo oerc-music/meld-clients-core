79414b0f8db5015b4841d30fa56f8e1208689205
David M. Weigl <david.weigl@oerc.ox.ac.uk>
2017-08-25 12:19:37 +0100

    Fix bug with multiple annotations in archive view; slight restyle

--- containers/annotationsListing.js
+++ containers/annotationsListing.js
@@ -11,36 +11,43 @@ class AnnotationsListing extends Component {
 
 	render() { 
 		if(Object.keys(this.props.score).length && Object.keys(this.props.score.componentTargets).length) { 
+			// filter out undefined annotations
 			const anno = annotations.filter( (annotation) => { return annotation })
+			// order by timestamp
+			anno.sort(function(a, b) { return a["dct:created"] < b["dct:created"] ? -1 : 1 })
+			console.log("--Annotations: ", anno)
+			console.log("--Props: ", this.props)
 			return ( 
 				<div className="annotationsWrapper">
 					<img src="../../images/climbMap.jpg" width="300px"/>
+					<div>Events:</div>
 					{
-						// filter out undefined annotations
 						anno.map( (annotation) => {
 							return (
 								<div className="annotationListing" key={annotation["@id"]}>
-									<div>Events:</div>
 									<div className="annotationTarget">
 										{ 
 											annotation["oa:hasTarget"].map( 
-												(t) => { return (
-													<span 
-														className = {annotation["oa:motivatedBy"]["@id"].replace(":", "_")} 
-														title={t["@id"]} 
-														key={t["@id"]}	
-														onClick={ () => { 
-															this.props.scorePageToComponentTarget(
-																// TODO: Implement "startsWith" to pick correct
-																// first MEI target in future
-																this.props.score.componentTargets[t["@id"]]["MEI"][0],
-																this.props.scoreUri,
-																this.props.score.MEI[this.props.scoreUri]
-															)
-														}}>
-																{this.props.score.componentTargets[t["@id"]]["description"]}
-														</span> 
-												)}
+												(t) => { 
+													if(t["@id"] in this.props.score.componentTargets) { return (
+														<span 
+															className = {annotation["oa:motivatedBy"]["@id"].replace(":", "_")} 
+															title={t["@id"]} 
+															key={t["@id"]}	
+															onClick={ () => { 
+																this.props.scorePageToComponentTarget(
+																	// TODO: Implement "startsWith" to pick correct
+																	// first MEI target in future
+																	this.props.score.componentTargets[t["@id"]]["MEI"][0],
+																	this.props.scoreUri,
+																	this.props.score.MEI[this.props.scoreUri]
+																)
+															}}>
+																	{this.props.score.componentTargets[t["@id"]]["description"]}
+															</span> 
+													)}
+													else { return <span>Loading component target: {t["@id"]}</span> }
+												}
 											)  
 										}
 										<span className="timestamp" key={annotation["@id"] + "_time"}> 
